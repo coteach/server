38e81f05034850848bd5615b623eda563e0ea352
Kahou <kahou@ducker.app>
2020-09-20 17:43:31 +0800

    feat(scraper): write plans in one file

--- services/scraper/main.py
+++ services/scraper/main.py
@@ -1,26 +1,36 @@
 import asyncio
 
+from models.plan import Plan
 from plan_store import PlanStore
 from registeration import *
 
 
 async def update_files():
+    await PlanStore.open_plans_file()
     await asyncio.gather(
         *[update_file(value) for value in Origin], update_origin_file()
     )
+    await PlanStore.close_plans_file()
 
 
-async def update_file(orgin: Origin):
-    scraper = orgin.create_scraper()
+
+async def update_file(origin: Origin):
+    plans = await merge_plans(origin)
+    json_string = await PlanStore.write_original_plans(origin.name, plans)
+    await PlanStore.write_plans(json_string)
+
+
+async def merge_plans(origin: Origin) -> [Plan]:
+    scraper = origin.create_scraper()
     scraper_task = asyncio.create_task(scraper.scrape())
-    file_plans = await PlanStore.read(orgin.name)
+    file_plans = await PlanStore.read_original_plans(origin.name)
     web_plans = await scraper_task
 
     for web_plan in web_plans:
         if web_plan.name not in file_plans.keys():
             file_plans[web_plan.name] = web_plan
 
-    await PlanStore.write(orgin.name, file_plans.values())
+    return file_plans.values()
 
 
 async def update_origin_file():
--- services/scraper/plan_store.py
+++ services/scraper/plan_store.py
@@ -1,5 +1,6 @@
 import json
 import os
+from enum import Enum, auto
 from string import Template
 from typing import Final
 
@@ -10,8 +11,10 @@ PATH: Final[str] = "outputs/$file_name.json"
 
 
 class PlanStore:
+    _iswriting: bool
+
     @classmethod
-    async def read(cls, file_name: str) -> dict:
+    async def read_original_plans(cls, file_name: str) -> dict:
         path = cls.get_path(file_name)
         if not os.path.isfile(path):
             return {}
@@ -22,10 +25,32 @@ class PlanStore:
             return {plan.name: plan for plan in plans}
 
     @classmethod
-    async def write(cls, file_name: str, plans: [Plan]):
+    async def write_original_plans(cls, file_name: str, plans: [Plan]) -> str:
+        json_string = json.dumps([plan.to_dict() for plan in plans], ensure_ascii=False)
         with open(cls.get_path(file_name), "w") as file:
-            data = [plan.to_dict() for plan in plans]
-            json.dump(data, file, ensure_ascii=False)
+            file.write(json_string)
+
+        return json_string
+
+    @classmethod
+    async def open_plans_file(cls) -> str:
+        cls._iswriting = False
+        with open(cls.get_path("plans"), "w") as file:
+            file.write("[")
+
+    @classmethod
+    async def write_plans(cls, json_string: str) -> str:
+        with open(cls.get_path("plans"), "a") as file:
+            if cls._iswriting:
+                file.write(",")
+            else:
+                cls._iswriting = True
+            file.write(json_string[1:-1])
+
+    @classmethod
+    async def close_plans_file(cls) -> str:
+        with open(cls.get_path("plans"), "a") as file:
+            file.write("]")
 
     @classmethod
     async def write_origin(cls, origins: [PlanOrigin]):
@@ -34,5 +59,5 @@ class PlanStore:
             json.dump(data, file, ensure_ascii=False)
 
     @classmethod
-    def get_path(cls, file_name: str):
+    def get_path(cls, file_name: str) -> str:
         return Template(PATH).substitute(file_name=file_name)
