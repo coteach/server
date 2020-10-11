72bdaa619f03ffcc10322c03ee0364007d8a5736
Nesl amd <neslxzhen@gmail.com>
2020-10-11 19:17:51 +0800

    fix(scraper): UnicodeDecodeError

--- services/scraper/plan_store.py
+++ services/scraper/plan_store.py
@@ -19,7 +19,7 @@ class PlanStore:
         if not os.path.isfile(path):
             return {}
 
-        with open(path) as file:
+        with cls.open_file(file_name, "r") as file:
             data = json.load(file)
             plans = [Plan.from_dict(dict) for dict in data]
             return {plan.name: plan for plan in plans}
@@ -27,7 +27,7 @@ class PlanStore:
     @classmethod
     async def write_original_plans(cls, file_name: str, plans: [Plan]) -> str:
         json_string = json.dumps([plan.to_dict() for plan in plans], ensure_ascii=False)
-        with open(cls.get_path(file_name), "w") as file:
+        with cls.open_file(file_name, "w") as file:
             file.write(json_string)
 
         return json_string
@@ -35,12 +35,12 @@ class PlanStore:
     @classmethod
     async def open_plans_file(cls) -> str:
         cls._iswriting = False
-        with open(cls.get_path("plans"), "w") as file:
+        with cls.open_file("plans", "w") as file:
             file.write("[")
 
     @classmethod
     async def write_plans(cls, json_string: str) -> str:
-        with open(cls.get_path("plans"), "a") as file:
+        with cls.open_file("plans", "a") as file:
             if cls._iswriting:
                 file.write(",")
             else:
@@ -49,15 +49,19 @@ class PlanStore:
 
     @classmethod
     async def close_plans_file(cls) -> str:
-        with open(cls.get_path("plans"), "a") as file:
+        with cls.open_file("plans", "a") as file:
             file.write("]")
 
     @classmethod
     async def write_origin(cls, origins: [PlanOrigin]):
-        with open(cls.get_path("origin"), "w") as file:
+        with cls.open_file("origin", "w") as file:
             data = [origin.to_dict() for origin in origins]
             json.dump(data, file, ensure_ascii=False)
 
     @classmethod
     def get_path(cls, file_name: str) -> str:
         return Template(PATH).substitute(file_name=file_name)
+
+    @classmethod
+    def open_file(cls, file_name: str, mode: str) -> str:
+        return open(cls.get_path(file_name), mode, encoding="utf-8")
